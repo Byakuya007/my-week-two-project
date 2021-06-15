ae921fe8f6380fb50a659b28fc3dcd21406f6f10
Byakuya007 <denniscollinsthumbi007@gmail.com>
2021-06-15 08:35:25 +0300

    commit css file changes

---
+++ jason.js
@@ -0,0 +1,8 @@
+var myDates;
+myDates = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var myMalename;
+myMalenames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+var myFemalenames;
+myFemalenames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
