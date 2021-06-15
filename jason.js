26dd90b91ef11ec6f4425845ace5e64fbe0478c6
Byakuya007 <denniscollinsthumbi007@gmail.com>
2021-06-15 15:40:55 +0300

    commit my js new codes

--- jason.js
+++ jason.js
@@ -1,9 +1,60 @@
-const items = [
-    { name: 'Kwasi', days: Sunday },
-    { name: 'Kwado', days: Monday },
-    { name: 'Kwabena', days: Tuesday },
-    { name: 'Kwaku', days: Wednesday },
-    { name: 'Yaw', days: Thursday },
-    { name: 'Kofi', days: Friday },
-    { name: 'Kwane', days: Saturday }
-]
+let daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+
+// alert("hello")
+
+let formValidation = function() {
+    // hideElement();
+    let dateData = document.forms["akanEntry"]["data"].value
+    let genderData = document.forms["akanEntry"]["gender"].value
+
+    if (dateData == "" || genderData == "") {
+        alert("Make sure all inputs are entered correctly to get your Akan name")
+        return false;
+    } else {
+
+        getUserDetails();
+    }
+}
+
+let getUserDetails = function() {
+    let data = document.getElementById("data").value;
+    let gender = document.getElementById("gender").value
+    let genDate = new Date(data);
+    genDate = genDate.getDay();
+    alert(genDate);
+    getAkanName(genDate, gender)
+}
+
+let display = document.getElementById("display")
+
+let getAkanName = function(day, gender) {
+    if (gender === "Male") {
+        alert(maleAkanNames[day])
+        alert(daysArray[day])
+            // display.innerHTML = "Hello" + maleAkanNames[day]
+        display.innerHTML = `Your Akan name is <strong> ${maleAkanNames[day]}</strong>  becuase you are a ${gender} born on ${daysArray[day]}`
+
+    } else if (gender == "Female") {
+        display.innerHTML = `Your Akan name is <strong> ${femaleAkanNames[day]} </strong>  becuase you are a ${gender} born on ${daysArray[day]}`
+    }
+}
+
+// let hideElement = function() {
+//     var x = document.getElementById(" hidden-display");
+//     x.style.display = "block";
+
+// }
+
+let unhideElement = function() {
+
+    var x = document.getElementById("hidden-display");
+    x.style.display = "none";
+
+}
+
+let reset = function() {
+    location.reload();
+}
